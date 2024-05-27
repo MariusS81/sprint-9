77cda6c803b2288e3f71bcc4f324e339bf7c182f
MariusS81 <mariusvilaescu@yahoo.com>
2024-05-27 12:47:15 +0300

    ex4 completed

---
+++ ex4/index.js
@@ -0,0 +1,6 @@
+const addNumber = (...argumente) => argumente.reduce((suma, numar) => suma + numar, 0);
+
+console.log(addNumber(1, 2, 3));         
+console.log(addNumber(1, 2, 3, 4, 5));    
+console.log(addNumber(10, -2, 3, 7));    
+console.log(addNumber());  
